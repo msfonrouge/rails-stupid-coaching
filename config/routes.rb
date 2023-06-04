e198019c63f68810c3a344e712959a1652000893
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2023-06-04 19:09:57 -0300

    Solve stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root to: "questions#ask"
+    get "ask", to: "questions#ask"
+    get "answer", to: "questions#answer"
+  end
 end
